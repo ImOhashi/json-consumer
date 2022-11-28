621476525caa886b7e5000b9e4bebe906128ab50
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-27 23:40:37 -0300

    add json consumer

---
+++ src/main/java/com/ohashi/jsonconsumer/listener/JsonListener.java
@@ -0,0 +1,19 @@
+package com.ohashi.jsonconsumer.listener;
+
+import com.ohashi.jsonconsumer.model.Payment;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class JsonListener {
+
+    @SneakyThrows
+    @KafkaListener(topics = "payment-topic", groupId = "create-group", containerFactory = "jsonContainerFactory")
+    public void antiFraud(@Payload Payment payment) {
+        log.info("message: {}", payment.toString());
+    }
+}
